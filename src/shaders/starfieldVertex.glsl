6f8f8f114d12768147000e6e6082dc4325a806b0
PythonChile <125165455+PyCHile@users.noreply.github.com>
2026-08-12 10:06:21 -0400

    Pass star seed into cosmic color shader

--- src/shaders/starfieldVertex.glsl
+++ src/shaders/starfieldVertex.glsl
@@ -1,4 +1,6 @@
 attribute float aSize;
 attribute float aAlpha;
+attribute float aSeed;
 varying float vAlpha;
-void main(){vec4 mv=modelViewMatrix*vec4(position,1.0);float perspective=clamp(120.0/max(4.0,-mv.z),0.45,5.0);gl_PointSize=aSize*perspective;vAlpha=aAlpha;gl_Position=projectionMatrix*mv;}
+varying float vSeed;
+void main(){vec4 mv=modelViewMatrix*vec4(position,1.0);float perspective=clamp(120.0/max(4.0,-mv.z),0.45,5.0);gl_PointSize=aSize*perspective;vAlpha=aAlpha;vSeed=aSeed;gl_Position=projectionMatrix*mv;}
