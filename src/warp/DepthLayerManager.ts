bf58feb63fa2ebf7ea243e8d0b697a0415bed0a1
PythonChile <125165455+PyCHile@users.noreply.github.com>
2026-08-12 16:17:37 -0400

    Mirror compressed center star density in source

--- src/warp/DepthLayerManager.ts
+++ src/warp/DepthLayerManager.ts
@@ -1,1 +1,1 @@
-export class DepthLayerManager{sampleDepth(r:number,depth:number){return-8-Math.pow(r,.72)*(depth-8);}sampleRadius(r:number){return 1.4+Math.pow(r,.60)*30;}}
+export class DepthLayerManager{sampleDepth(r:number,depth:number){return-8-Math.pow(r,.72)*(depth-8);}sampleRadius(r:number){return .55+Math.pow(r,1.72)*31.5;}}
