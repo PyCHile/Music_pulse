83df672036402fa6017c0bc9cf4140caa1e40110
PythonChile <125165455+PyCHile@users.noreply.github.com>
2026-08-13 14:30:36 -0400

    release: URUX 2026.08.13.50 cinematic recovery

--- runtime/journey/CinematicEncounterSystemV42.js
+++ runtime/journey/CinematicEncounterSystemV42.js
@@ -1,14 +1,1 @@
-const mobile=/iPad|iPhone|iPod|Android/i.test(navigator.userAgent||'')||innerWidth<800;
-let CinematicEncounterSystem;
-if(mobile){
- ({CinematicEncounterSystem}=await import('./MobileCinematicEncounterSystem.js?v=20260813-47'));
-}else{
- const [{CinematicEncounterSystem:BaseCinematicEncounterSystem},{AstronomicalTextureManager}]=await Promise.all([
-  import('./CinematicEncounterSystem.js?v=20260813-46'),
-  import('../assets/AstronomicalTextureManager.js?v=20260813-42')
- ]);
- CinematicEncounterSystem=class DesktopCinematicEncounterSystem extends BaseCinematicEncounterSystem{
-  constructor(scene,renderer=null){super(scene,null);this.mobilePerformanceProfile=false;if(renderer){this.textureManager=new AstronomicalTextureManager(renderer);this.stats.textures={...this.textureManager.stats};}}
- };
-}
-export { CinematicEncounterSystem };
+const m=/iPad|iPhone|iPod|Android/i.test(navigator.userAgent||'')||innerWidth<800;export const CinematicEncounterSystem=m?(await import('./MobileCinematicEncounterSystemV50.js?v=20260813-50')).CinematicEncounterSystem:(await import('./CinematicEncounterSystem.js?v=20260813-46')).CinematicEncounterSystem;
