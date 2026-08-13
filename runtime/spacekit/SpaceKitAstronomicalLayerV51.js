85c36acbe3877c127332edbb306c775ba6d5b364
PythonChile <125165455+PyCHile@users.noreply.github.com>
2026-08-13 18:22:15 -0400

    perf: reserve SpaceKit for lab mode outside production renderer

---
+++ runtime/spacekit/SpaceKitAstronomicalLayerV51.js
@@ -0,0 +1,5 @@
+export class SpaceKitAstronomicalLayer{
+ constructor(){this.ready=false;this.failed=false;this.disabledForPerformance=true;this.usesLiteralSpaceKit=false;this.activePlanetCount=0;this.closestPlanetZ=-999999;this.focalOriginError=0;this.lateralOffset=0;this.flybyProgress=0;}
+ update(){}
+ resize(){}
+}
